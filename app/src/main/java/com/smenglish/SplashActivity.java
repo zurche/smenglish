bb6f1cf89f984210c2842c1d2769100f832cef93
Alejandro Zurcher <alejandro.zurcher@gmail.com>
2017-02-05 15:15:26 -0300

    Adding logging message to login screen

--- app/src/main/java/com/smenglish/SplashActivity.java
+++ app/src/main/java/com/smenglish/SplashActivity.java
@@ -1,9 +1,11 @@
 package com.smenglish;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
@@ -16,12 +18,17 @@ import com.facebook.login.widget.LoginButton;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends AppCompatActivity {
 
     private static final String TAG = SplashActivity.class.getSimpleName();
 
     @BindView(R.id.login_button)
     LoginButton login_button;
+
+    @BindView(R.id.unlogged_user_message)
+    LinearLayout unlogged_user_message;
+    @BindView(R.id.logging_user_message)
+    LinearLayout logging_user_message;
 
     private CallbackManager callbackManager;
 
@@ -32,6 +39,14 @@ public class SplashActivity extends Activity {
         ButterKnife.bind(this);
 
         callbackManager = CallbackManager.Factory.create();
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logging_user_message.setVisibility(View.VISIBLE);
+                unlogged_user_message.setVisibility(View.GONE);
+            }
+        });
 
         login_button.setReadPermissions("email");
         login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -45,12 +60,16 @@ public class SplashActivity extends Activity {
             public void onCancel() {
                 Log.d(TAG, "Cancelled facebook login");
                 Toast.makeText(SplashActivity.this, "Error en login a facebook", Toast.LENGTH_SHORT).show();
+                logging_user_message.setVisibility(View.GONE);
+                unlogged_user_message.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onError(FacebookException exception) {
                 Log.e(TAG, "Error facebook login", exception);
                 Toast.makeText(SplashActivity.this, "Error en login a facebook", Toast.LENGTH_SHORT).show();
+                logging_user_message.setVisibility(View.GONE);
+                unlogged_user_message.setVisibility(View.VISIBLE);
             }
         });
 
